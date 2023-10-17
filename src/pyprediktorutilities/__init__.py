903ccdab7036cfffa8bd5648b796fd5911f34d03
Andreas Lydersen <andreas.lydersen@prediktor.com>
2023-10-17 09:21:53 +0200

    Removing 3.8 adding 3.11

--- src/pyprediktorutilities/__init__.py
+++ src/pyprediktorutilities/__init__.py
@@ -1,8 +1,8 @@
 import sys
 from .send_email import *
 
-if sys.version_info[:2] >= (3, 8):
-    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
+if sys.version_info[:2] >= (3, 9):
+    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.9`
     from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
 else:
     from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
