98d1c7a46f15fe4c8d2f694fbce95e8d2a6f751b
Pavel Tashev <pta>
2025-01-08 12:30:32 +0500

    PENGSOL-555 When executing a query or fetching data from the database, always use try-finally. Inside finally block always execute disconnect

--- src/pyprediktorutilities/dwh/dwh.py
+++ src/pyprediktorutilities/dwh/dwh.py
@@ -98,23 +98,25 @@ class Dwh:
                 is going to be in DataFrame format.
         """
         self.__connect()
-        self.cursor.execute(query)
+        try:
+            self.cursor.execute(query)
 
-        data_sets = []
-        while True:
-            columns = [col[0] for col in self.cursor.description]
-            data_set = [dict(zip(columns, row)) for row in self.cursor.fetchall()]
+            data_sets = []
+            while True:
+                columns = [col[0] for col in self.cursor.description]
+                data_set = [dict(zip(columns, row)) for row in self.cursor.fetchall()]
 
-            if to_dataframe:
-                data_sets.append(pd.DataFrame(data_set, columns=columns))
-            else:
-                data_sets.append(data_set)
+                if to_dataframe:
+                    data_sets.append(pd.DataFrame(data_set, columns=columns))
+                else:
+                    data_sets.append(data_set)
 
-            if not self.cursor.nextset():
-                break
+                if not self.cursor.nextset():
+                    break
 
-        self.__disconnect()  # prevent from leaving open transactions in DWH
-        return data_sets if len(data_sets) > 1 else data_sets[0]
+            return data_sets if len(data_sets) > 1 else data_sets[0]
+        finally:
+            self.__disconnect()  # prevent from leaving open transactions in DWH
 
     @validate_call
     def execute(self, query: str, *args, **kwargs) -> List[Any]:
@@ -142,15 +144,17 @@ class Dwh:
         """
         self.__connect()
         try:
-            self.cursor.execute(query, *args, **kwargs)
-            result = self.cursor.fetchall()
-        except Exception as e:
-            logging.error(f"Failed to execute query: {e}")
-            return []
+            try:
+                self.cursor.execute(query, *args, **kwargs)
+                result = self.cursor.fetchall()
+            except Exception as e:
+                logging.error(f"Failed to execute query: {e}")
+                return []
 
-        self.__commit()
-        self.__disconnect()  # prevent from leaving open transactions in DWH
-        return result
+            self.__commit()
+            return result
+        finally:
+            self.__disconnect()  # prevent from leaving open transactions in DWH
 
     """
     Private - Driver
